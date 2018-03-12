a6aeb3ed2306e4abac9bfa2053849cf730c15673
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2018-03-12 23:04:07 +0100

    menu

--- app/src/main/java/com/mieczkowskidev/losoweslowko/MainActivity.kt
+++ app/src/main/java/com/mieczkowskidev/losoweslowko/MainActivity.kt
@@ -2,11 +2,17 @@ package com.mieczkowskidev.losoweslowko
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        return super.onCreateOptionsMenu(menu)
     }
 }
