79f9265d15c74d471cce60af9865d54f6fdcc8d6
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2018-03-15 00:15:51 +0100

    added activity

--- app/src/main/java/com/mieczkowskidev/losoweslowko/FullscreenActivity.kt
+++ app/src/main/java/com/mieczkowskidev/losoweslowko/FullscreenActivity.kt
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.view.View
-import kotlinx.android.synthetic.main.activity_fullscreen.*
+import kotlinx.android.synthetic.main.activity_fullscreen.*     
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
