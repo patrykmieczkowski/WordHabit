4d5a00adbe51370cfd09b9b889d452157578f21e
Patryk Mieczkowski <mieczkowskidev@gmail.com>
2018-03-21 19:06:37 +0100

    changes

--- app/src/main/java/com/mieczkowskidev/wordhabit/MainActivity.kt
+++ app/src/main/java/com/mieczkowskidev/wordhabit/MainActivity.kt
@@ -18,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         private val TAG = MainActivity::class.java.simpleName
     }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         super.onCreate(savedInstanceState)
